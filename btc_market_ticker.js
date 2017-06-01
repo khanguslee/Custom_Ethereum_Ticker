21622c712e2462092951dc59c189705e4f8ae754
khanguslee <angus.lee809@gmail.com>
2017-06-01 13:27:23 +1000

    Added a cleaner output

--- btc_market_ticker.js
+++ btc_market_ticker.js
@@ -3,9 +3,14 @@ const url = 'https://api.btcmarkets.net/market/ETH/AUD/tick';
 
 function sendRequest(){
     request(url, function (error, response, body) {
-        console.log(body);
+        //console.log(body);    // Display body of return message
         body = JSON.parse(body);
-        
+        console.log("Crypto currency: ", body.instrument);
+        console.log("Currency: ", body.currency);
+        console.log("Best bid: ", body.bestBid);
+        console.log("Best ask: ", body.bestAsk);
+        console.log("Last price: ", body.lastPrice);
+        console.log();
     });
 }
 

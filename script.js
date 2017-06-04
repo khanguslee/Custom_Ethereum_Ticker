5751945a352ecd4eb3a982540ed8bff6f73da91f
khanguslee <angus.lee809@gmail.com>
2017-06-05 00:25:34 +1000

    Added ability to input the amount of Ether you have to determine the current value of it.

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ socket.on('updatePrice', function(data){
   var currentTime = new Date().getTime();
   currentTime = Math.floor(currentTime/1000);
   updateTime = data[5];
-  
+
   // Update HTML fields
   document.getElementById('currentCryptoCurrency').innerHTML = data[0];
   document.getElementById('currentActualCurrency').innerHTML = data[1];
@@ -16,9 +16,19 @@ socket.on('updatePrice', function(data){
   document.getElementById('currentAsk').innerHTML = data[3];
   document.getElementById('currentLastPrice').innerHTML = data[4];
   document.getElementById('lastUpdateTime').innerHTML = currentTime - updateTime;
-
+  if (document.getElementById('outputValue').textContent != 'N/A'){
+    submitValue();
+  }
 
 })
+
+function submitValue(){
+  var prevAmount, totalAmount;
+  etherAmount = document.getElementById('inputValue').value;
+  etherPrice = document.getElementById('currentLastPrice').textContent;
+  totalAmount = (etherAmount*etherPrice).toFixed(2);
+  document.getElementById('outputValue').innerHTML = totalAmount;
+}
 /*
 var myChart = new Chart(ctx, {
   type: 'line',
